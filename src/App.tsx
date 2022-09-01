497f880699209c939fdeac3af6628ad0d2067547
Tim Koepsel <xzesstence@gmail.com>
2022-09-01 17:07:10 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={'/assets/images/favicon.png'} className="App-logo" alt="logo" />
+        <h2>
+          <img src={'/assets/images/logo.png'} alt="logo2" /> Boilerplate
+        </h2>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
